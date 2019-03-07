55781b0559d1b11bbb357d84d2086330b2099ad4
EC2 Default User <ec2-user@ip-172-31-0-98.us-east-2.compute.internal>
2019-03-07 22:38:48 +0000

    Part I comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  #map similar_movie to call similar method in controller
   get '/movies/:id/similar', to: 'movies#similar',as: 'similar_movie'
 end
